144bff261052c1e239aad70f86bda018f81d9736
bryanLR <bledesma2.optimus@yobelscm.biz>
2025-04-16 17:53:59 -0500

    metodo de service y repository para generar reporte PDF programa de cargas supervisor

--- src/main/java/com/pdf_reports/application/services/impl/SupervisorService.java
+++ src/main/java/com/pdf_reports/application/services/impl/SupervisorService.java
@@ -6,20 +6,36 @@ import com.pdf_reports.domain.repositories.ISupervisorRepository;
 import com.pdf_reports.utils.constants.messages.ErrorMessage;
 import com.pdf_reports.utils.exceptions.CargoDoesNotExistException;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class SupervisorService implements ISupervisorService {
     private final ISupervisorRepository repository;
+    private final DataSource dataSource;
 
     @Override
     public byte[] generateSupervisorReport(SupervisorRequest request) {
-        return new byte[0];
-    }
+        try(Connection cn = dataSource.getConnection()) {
+            InputStream reportStream = getResource("/reports/loadScheduleSupervisor/loadSchedule.jasper");
+            InputStream banner = getResource("/reports/yobelbanner.png");
+            JasperPrint jasperPrint = JasperFillManager.fillReport(reportStream, setParameters(banner), cn);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }    }
 
     @Override
     public void ensureAnyCargoExists(SupervisorRequest request) {
@@ -29,7 +45,17 @@ public class SupervisorService implements ISupervisorService {
         int cargoQuantity = repository.validate(request.account(), processDate.toLocalDateTime(), request.processBatch());
 
         if(cargoQuantity == 0) {
-            throw new CargoDoesNotExistException(ErrorMessage.NO_CARGOS_FOUND.value());
+            throw new CargoDoesNotExistException(ErrorMessage.NO_ORDERS_FOUND.value());
         }
+    }
+
+    private InputStream getResource(String path) {
+        return this.getClass().getResourceAsStream(path);
+    }
+
+    public Map<String, Object> setParameters(InputStream banner) {
+        Map<String, Object> params = new HashMap<>();
+
+        return params;
     }
 }
--- src/main/java/com/pdf_reports/utils/constants/messages/ErrorMessage.java
+++ src/main/java/com/pdf_reports/utils/constants/messages/ErrorMessage.java
@@ -3,7 +3,8 @@ package com.pdf_reports.utils.constants.messages;
 public enum ErrorMessage {
     CARGO_DOES_NOT_EXIST("Carga no existe para el CD y fecha ingresados"),
     NO_CARGOS_FOUND("No existen cargas para el CD y fecha ingresados"),
-    STATUS_DOES_NOT_EXIST("El status enviado no es válido");
+    STATUS_DOES_NOT_EXIST("El status enviado no es válido"),
+    NO_ORDERS_FOUND("No existen ordenes para los parámetros ingresadis");
     private final String description;
 
     ErrorMessage(String description) {
