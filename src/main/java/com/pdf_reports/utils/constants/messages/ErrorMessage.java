6f0e8aea0e4d8a16d3707021bda48591a6a35717
bryanLR <bledesma2.optimus@yobelscm.biz>
2025-04-29 10:20:21 -0500

    corrección mensaje de error y archivo jasper del reporte

--- src/main/java/com/pdf_reports/utils/constants/messages/ErrorMessage.java
+++ src/main/java/com/pdf_reports/utils/constants/messages/ErrorMessage.java
@@ -4,7 +4,7 @@ public enum ErrorMessage {
     CARGO_DOES_NOT_EXIST("Carga no existe para el CD y fecha ingresados"),
     NO_CARGOS_FOUND("No existen cargas para el CD y fecha ingresados"),
     STATUS_DOES_NOT_EXIST("El status enviado no es válido"),
-    NO_ORDERS_FOUND("No existen ordenes para los parámetros ingresadis");
+    NO_ORDERS_FOUND("No existen ordenes para los parámetros ingresados");
     private final String description;
 
     ErrorMessage(String description) {
